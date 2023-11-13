c3bf2a9561ceec81030dcbd8f9f0fa168e46b5d9
manu <you@example.com>
2023-11-13 15:41:44 +0100

    Add echo

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,32 @@ fn handle_stream(stream: &mut TcpStream) -> Result<()> {
     let mut reader = BufReader::new(stream.try_clone()?);
     reader.read_line(&mut buff)?;
     dbg!(&buff);
-    let parts: Vec<_> = buff.split(' ').collect();
-    if parts.get(1) == Some(&"/") {
-        let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
-    } else {
-        let _ = stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
-    }
+    let response = handle_path(&buff)?;
+    let echo = build_content(&response);
+    let _ = stream.write(&echo.into_bytes());
+    // let parts: Vec<_> = buff.split(' ').collect();
+    // if parts.get(1) == Some(&"/") {
+    //     let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    // } else {
+    //     let _ = stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+    // }
     Ok(())
+}
+
+fn handle_path(path: &str) -> Result<String> {
+    let parts: Vec<_> = path.split('/').collect();
+    match parts.first() {
+        Some(&"echo") => Ok(parts.get(1).expect("nothing to echo").to_string()),
+        _ => unreachable!(),
+    }
+}
+
+fn build_content(content: &str) -> String {
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+        &content.len(),
+        &content
+    )
 }
 
 fn main() {
