ec7c6bd4356311223b2824667cfbb76599852a04
manu <you@example.com>
2023-11-14 17:42:11 +0100

    Add multiple returned type

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use anyhow::anyhow;
 use anyhow::Result;
+use std::env::args;
+use std::fs;
 use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::path::Path;
 use std::{thread, time};
 
-fn handle_stream(mut stream: TcpStream) -> Result<()> {
+fn handle_stream(mut stream: TcpStream, dir: &str) -> Result<()> {
     let mut buff = [0; 1024].to_vec();
     stream.read(&mut buff)?;
     let content = String::from_utf8(buff)?;
@@ -37,12 +40,16 @@ fn handle_stream(mut stream: TcpStream) -> Result<()> {
         }
     }
 
-    match handle_path(path, &user_agent) {
-        Ok(None) => {
+    match handle_path(path, &user_agent, dir) {
+        Ok(Served::Empty) => {
             let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
         }
-        Ok(Some(response)) => {
-            let echo = build_content(&response);
+        Ok(Served::String(response)) => {
+            let echo = build_content(&response, "text/plain");
+            let _ = stream.write(&echo.into_bytes());
+        }
+        Ok(Served::File(content)) => {
+            let echo = build_content(&content, "application/octet-stream");
             let _ = stream.write(&echo.into_bytes());
         }
         _ => {
@@ -52,27 +59,45 @@ fn handle_stream(mut stream: TcpStream) -> Result<()> {
     Ok(())
 }
 
-fn handle_path(path: &str, user_agent: &str) -> Result<Option<String>> {
+enum Served {
+    Empty,
+    String(String),
+    File(String),
+}
+
+fn handle_path(path: &str, user_agent: &str, dir: &str) -> Result<Served> {
     let parts: Vec<_> = path.split('/').collect();
-    // dbg!(&parts);
     match parts.get(1) {
-        Some(&"") => Ok(None),
+        Some(&"") => Ok(Served::Empty),
         Some(&"echo") => {
             if parts.len() < 2 {
-                Ok(None)
+                Ok(Served::Empty)
             } else {
                 let r = parts[2..parts.len()].join("/");
-                Ok(Some(r.clone()))
+                Ok(Served::String(r.clone()))
             }
         }
-        Some(&"user-agent") => Ok(Some(String::from(user_agent))),
+        Some(&"user-agent") => Ok(Served::String(String::from(user_agent))),
+        Some(&"files") => {
+            let r = parts[2..parts.len()].join("/");
+            handle_file(&r, dir)
+        }
         _ => Err(anyhow!("invalid path")),
     }
 }
 
-fn build_content(content: &str) -> String {
+fn handle_file(path: &str, dir: &str) -> Result<Served> {
+    let p = format!("{}/{}", dir, path);
+    let p = Path::new(&p);
+    let content = fs::read(p)?;
+    let content = String::from_utf8(content)?;
+    Ok(Served::File(content))
+}
+
+fn build_content(content: &str, content_type: &str) -> String {
     format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}\r\n",
+        &content_type,
         &content.len(),
         &content
     )
@@ -81,6 +106,16 @@ fn build_content(content: &str) -> String {
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let mut previous_arg = String::new();
+    let mut directory = String::new();
+    for arg in args() {
+        if previous_arg == "--directory" {
+            directory = arg;
+            break;
+        } else {
+            previous_arg = arg;
+        }
+    }
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -88,7 +123,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handlers.push(thread::spawn(|| match handle_stream(stream) {
+                let dir = directory.clone();
+                handlers.push(thread::spawn(move || match handle_stream(stream, &dir) {
                     Ok(o) => {
                         dbg!(o);
                     }
